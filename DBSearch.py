05f45b8ef5ce59cd0d2c1372f193b1dad3ad25f8
FanYucai <fanyucai@126.com>
2017-04-15 13:06:16 +0800

    it sucks

--- DBSearch.py
+++ DBSearch.py
@@ -2,8 +2,9 @@ import MySQLdb
 from Tkinter import *
 
 class App(object):
-
     def __init__(self, master):
+        self.col1Content = ''
+        self.col2Content = ''
         self.master = master
         self.col1 = Label(master, text="col1", borderwidth=2)
         self.col1.grid(row=1, sticky=W)
@@ -11,23 +12,23 @@ class App(object):
         self.col2 = Label(master, text="col2", borderwidth=2)
         self.col2.grid(row=2, sticky=W)
 
-        self.col1Entry = Entry(master)
+        self.col1Entry = Entry(master, textvariable=self.col1Content)
         self.col1Entry.grid(row=1, column=1, columnspan=2)
         self.col1Entry.focus_set()
 
-        self.col2Entry = Entry(master)
+        self.col2Entry = Entry(master, textvariable=self.col2Content)
         self.col2Entry.grid(row=2, column=1, columnspan=2)
 
         self.showSQL = 'qwq'
         self.searchButton = Button(master, text='search', borderwidth=2, command=self.searchDB)
         self.searchButton.grid(row=3, column=2)
 
-        self.sqlLabel = Label(master, text=self.showSQL, justify=CENTER)
+        self.sqlLabel = Label(master, textvariable=self.showSQL, justify=CENTER)
         self.sqlLabel.grid(row=0, columnspan=3)
 
     def searchDB(self):
-        self.col1Content = self.col1Entry.get().strip()
-        self.col2Content = self.col2Entry.get().strip()
+#        self.col1Content = self.col1Entry.get().strip()
+#        self.col2Content = self.col2Entry.get().strip()
         self.showSQL = self.col1Content + ' ' + self.col2Content
         return
 
@@ -49,6 +50,7 @@ class App(object):
 
 root = Tk()
 app = App(root)
+#root.bind('<Return>', App.searchDB)
 #entryCol1 = Entry(top)
 #entryCol2 = Entry(top)
 
