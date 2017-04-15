739b0e28ebdb9068a1d4324ff3f59b803e0d925c
FanYucai <fanyucai@126.com>
2017-04-15 12:22:06 +0800

    gui first test

--- DBSearch.py
+++ DBSearch.py
@@ -1,4 +1,35 @@
 import MySQLdb
+from Tkinter import *
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.inputSearch = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+            )
+        self.inputSearch.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print "hi there, everyone!"
+
+
+root = Tk()
+app = App(root)
+#entryCol1 = Entry(top)
+#entryCol2 = Entry(top)
+
+
+#entryCol1.pack()
+#entryCol2.pack()
+
+#### SQL part####
 
 db = MySQLdb.connect("localhost","root","12345678","labdb")
 
@@ -13,3 +44,7 @@ except:
     db.rollback()
 
 db.close()
+
+#### SQL part end ####
+
+root.mainloop()
