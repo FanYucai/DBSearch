4c64e55ae785b43cb494460729efe9717fad7185
FanYucai <fanyucai@126.com>
2017-04-25 21:18:27 +0800

    display finished, can't suit size

--- dbsearch2.py
+++ dbsearch2.py
@@ -18,7 +18,7 @@ class MyFrame(wx.Frame):
         
         # wx.StaticText(panelSname, -1, "Sname", pos = (13, 12))
         # wx.StaticText(panelSage, -1, "Sage", pos = (15, 12))
-        self.Ssex = wx.ComboBox(panel, -1, "", pos = (60, 100), size=(102,27), choices = [u"男", u"女", 'male']) 
+        self.Ssex = wx.ComboBox(panel, -1, "", pos = (60, 100), size=(102,27), choices = [u"男", u"女"]) 
         # self.rbox = wx.RadioBox(panel, label = u"性别", pos = (60,100), choices = [u"男", u"女"] , majorDimension = 1, style = wx.RA_SPECIFY_ROWS)
         self.Sid = wx.TextCtrl(panel, -1, "", pos = (60, 10))
         self.Sname = wx.TextCtrl(panel, -1, "", pos = (60, 40))
@@ -26,11 +26,24 @@ class MyFrame(wx.Frame):
         self.Sclass = wx.TextCtrl(panel, -1, "", pos = (250, 10))
         self.Sdept = wx.TextCtrl(panel, -1, "", pos = (250, 40))
         self.Saddr = wx.TextCtrl(panel, -1, "", pos = (250, 70))
+        self.ResDisplay = wx.TextCtrl(panel, -1, "", pos = (60, 200), size=(300, 50))
 
         self.sqlText = wx.TextCtrl(panel, -1, "", pos = (60, 140), size=(300, 50))
 
         self.buttonSearch = wx.Button(panel, label="Search", pos=(270, 100))
         self.buttonSearch.Bind(wx.EVT_BUTTON, self.searchSql)
+        
+        # sizer = wx.BoxSizer(wx.VERTICAL)
+        # sizer.Add(self.Sid,flag=wx.EXPAND)
+        # sizer.Add(self.Sname,flag=wx.EXPAND)
+        # sizer.Add(self.Sage,flag=wx.EXPAND)
+        # sizer.Add(self.Sclass,flag=wx.EXPAND)
+        # sizer.Add(self.Sdept,flag=wx.EXPAND)
+        # sizer.Add(self.Saddr,flag=wx.EXPAND)
+        # sizer.Add(self.sqlText,flag=wx.EXPAND)
+        # sizer.Add(self.ResDisplay,flag=wx.EXPAND)
+        # sizer.Add(self.buttonSearch,flag=wx.EXPAND)
+        # sizer.Add(self.Sname,flag=wx.EXPAND)
 
     def OnMove(self, event):
         pos = event.GetPosition()
@@ -47,59 +60,68 @@ class MyFrame(wx.Frame):
         self.ValSdept   = self.Sdept.GetValue() 
         self.ValSaddr   = self.Saddr.GetValue()
         self.ValSsex    = self.Ssex.GetValue()
-        print self.ValSsex
         
         hasPreAnd = 0
         if len(self.ValSid) != 0 :
-            sqlDisplay += " Sid="       + self.ValSid
+            sqlDisplay += " Sid=" + "'" + self.ValSid + "'"
             hasPreAnd = 1
         if len(self.ValSname) != 0:
             if hasPreAnd == 1:
                 sqlDisplay += " AND"
-            sqlDisplay += " Sname="     + self.ValSname
+            sqlDisplay += " Sname=" + "'" + self.ValSname + "'"
             hasPreAnd = 1
         if len(self.ValSage) != 0 :            
             if hasPreAnd == 1:
                 sqlDisplay += " AND"
-            sqlDisplay += " Sage="      + self.ValSage
+            sqlDisplay += " Sage=" + "'" + self.ValSage + "'"
             hasPreAnd = 1
         if len(self.ValSclass) != 0 :
             if hasPreAnd == 1:
                 sqlDisplay += " AND"
-            sqlDisplay += " Sclass="    + self.ValSclass
+            sqlDisplay += " Sclass=" + "'" + self.ValSclass + "'"
             hasPreAnd = 1
         if len(self.ValSdept) != 0 :
             if hasPreAnd == 1:
                 sqlDisplay += " AND"
-            sqlDisplay += " Sdept="     + self.ValSdept
+            sqlDisplay += " Sdept=" + "'" + self.ValSdept + "'"
             hasPreAnd = 1
         if len(self.ValSaddr) != 0 :
             if hasPreAnd == 1:
                 sqlDisplay += " AND"            
-            sqlDisplay += " Saddr="     + self.ValSaddr
+            sqlDisplay += " Saddr=" + "'" + self.ValSaddr + "'"
             hasPreAnd = 1
         if len(self.ValSsex) != 0 : 
             if hasPreAnd == 1:
                 sqlDisplay += " AND"           
-            sqlDisplay += " Ssex="  + self.ValSsex
+            sqlDisplay += " Ssex=" + "'" + self.ValSsex + "'"
+
+        sqlDisplay += ';'
 
         self.sqlText.SetValue(sqlDisplay)
         #search in MySQL:
        
-        db = MySQLdb.connect("localhost", "root", "12345678", "labdb")
+        db = MySQLdb.connect("localhost", "root", "12345678", "labdb", charset = 'utf8')
         cursor = db.cursor()
         sql = sqlDisplay
-        # sql = "select * from student"
-        # sql = "SELECT * FROM TESTTABLE WHERE testCol1=%d AND testCol2=%d" % (col1, col2)
+        # sql = sqlDisplay.encode('utf8')
+        print sql
         try:
             cursor.execute(sql)
             # db.commit()
             results = cursor.fetchall()
+            resDisplayValue = ""
             print ("共%d条查询结果\n") % len(results)
-            # for row in results:
-            #     resCol1 = row[0]
-            #     resCol2 = row[1]
-            #     print "row[1] = %d, row[2] = %d\n" % (resCol1, resCol2)    
+            for row in results:
+                resSid    = row[0]          + "\t"
+                resSage   = unicode(row[1]) + "\t"
+                resSsex   = row[2]          + "\t"
+                resSdept  = row[3]          + "\t"
+                resSname  = row[4]          + "\t"
+                resSaddr  = row[5]          + "\t"
+                resSclass = row[6]          + "\n"
+                resDisplayValue += resSid+resSage+resSsex+resSdept+resSname+resSaddr+resSclass
+                # print resDisplayValue
+            self.ResDisplay.SetValue(resDisplayValue)
         except:
             print "Error: unable to fecth data"
 
