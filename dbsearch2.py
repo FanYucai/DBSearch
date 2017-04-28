0af970a09a12bebb78511d480ac8e21df2902668
FanYucai <fanyucai@126.com>
2017-04-28 21:06:34 +0800

    init gender setting

--- dbsearch2.py
+++ dbsearch2.py
@@ -2,33 +2,35 @@
 # -*- coding:utf-8 -*-
 import MySQLdb
 import wx
+
+
 class MyFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, None, -1, "My Frame", size = (600, 400))
+        wx.Frame.__init__(self, None, -1, "My Frame", size=(670, 400))
         panel = wx.Panel(self, -1)
         # panel.Bind(wx.EVT_MOTION, self.OnMove)
-        wx.StaticText(panel, -1, u"学号", pos = (10, 10))
-        wx.StaticText(panel, -1, u"姓名", pos = (10, 40))
-        wx.StaticText(panel, -1, u"年龄", pos = (10, 70))
-        wx.StaticText(panel, -1, u"性别", pos = (10, 100))
+        wx.StaticText(panel, -1, u"学号", pos=(10, 10))
+        wx.StaticText(panel, -1, u"姓名", pos=(10, 40))
+        wx.StaticText(panel, -1, u"年龄", pos=(10, 70))
+        wx.StaticText(panel, -1, u"性别", pos=(10, 100))
 
-        wx.StaticText(panel, -1, u"班级", pos = (200, 10))
-        wx.StaticText(panel, -1, u"学院", pos = (200, 40))
-        wx.StaticText(panel, -1, u"地址", pos = (200, 70))
+        wx.StaticText(panel, -1, u"班级", pos=(200, 10))
+        wx.StaticText(panel, -1, u"学院", pos=(200, 40))
+        wx.StaticText(panel, -1, u"地址", pos=(200, 70))
 
-        # wx.StaticText(panelSname, -1, "Sname", pos = (13, 12))
-        # wx.StaticText(panelSage, -1, "Sage", pos = (15, 12))
-        self.Ssex = wx.ComboBox(panel, -1, "", pos = (60, 100), size=(102,27), choices = [u"男", u"女", u"-"])
-        # self.rbox = wx.RadioBox(panel, label = u"性别", pos = (60,100), choices = [u"男", u"女"] , majorDimension = 1, style = wx.RA_SPECIFY_ROWS)
-        self.Sid = wx.TextCtrl(panel, -1, "", pos = (60, 10))
-        self.Sname = wx.TextCtrl(panel, -1, "", pos = (60, 40))
-        self.Sage = wx.TextCtrl(panel, -1, "", pos = (60, 70))
-        self.Sclass = wx.TextCtrl(panel, -1, "", pos = (250, 10))
-        self.Sdept = wx.TextCtrl(panel, -1, "", pos = (250, 40))
-        self.Saddr = wx.TextCtrl(panel, -1, "", pos = (250, 70))
-        self.ResDisplay = wx.TextCtrl(panel, -1, "", pos = (60, 200), size=(550, 100), style=wx.TE_MULTILINE)
+        # wx.StaticText(panelSname, -1, "Sname", pos=(13, 12))
+        # wx.StaticText(panelSage, -1, "Sage", pos=(15, 12))
+        self.Ssex = wx.ComboBox(panel, -1, "", pos=(60, 100), size=(102, 27), choices=[u"-", u"男", u"女"])
+        # self.rbox = wx.RadioBox(panel, label = u"性别", pos=(60,100), choices = [u"男", u"女"] , majorDimension = 1, style = wx.RA_SPECIFY_ROWS)
+        self.Sid = wx.TextCtrl(panel, -1, "", pos=(60, 10))
+        self.Sname = wx.TextCtrl(panel, -1, "", pos=(60, 40))
+        self.Sage = wx.TextCtrl(panel, -1, "", pos=(60, 70))
+        self.Sclass = wx.TextCtrl(panel, -1, "", pos=(250, 10))
+        self.Sdept = wx.TextCtrl(panel, -1, "", pos=(250, 40))
+        self.Saddr = wx.TextCtrl(panel, -1, "", pos=(250, 70))
+        self.ResDisplay = wx.TextCtrl(panel, -1, "", pos=(60, 200), size=(550, 100), style=wx.TE_MULTILINE)
 
-        self.sqlText = wx.TextCtrl(panel, -1, "", pos = (60, 140), size=(550, 50))
+        self.sqlText = wx.TextCtrl(panel, -1, "", pos=(60, 140), size=(550, 50))
 
         self.buttonSearch = wx.Button(panel, label=u"查询", pos=(270, 100))
         self.buttonSearch.Bind(wx.EVT_BUTTON, self.searchSql)
@@ -47,22 +49,22 @@ class MyFrame(wx.Frame):
 
     def OnMove(self, event):
         pos = event.GetPosition()
-        self.Sid.SetValue("%s, %s" %(pos.x, pos.y))
+        self.Sid.SetValue("%s, %s" % (pos.x, pos.y))
 
     def searchSql(self, event):
-        #display:
+        # display:
         sqlDisplay = "SELECT * FROM Student WHERE"
 
-        self.ValSid     = self.Sid.GetValue()
-        self.ValSname   = self.Sname.GetValue()
-        self.ValSage    = self.Sage.GetValue()
-        self.ValSclass  = self.Sclass.GetValue()
-        self.ValSdept   = self.Sdept.GetValue()
-        self.ValSaddr   = self.Saddr.GetValue()
-        self.ValSsex    = self.Ssex.GetValue()
+        self.ValSid = self.Sid.GetValue()
+        self.ValSname = self.Sname.GetValue()
+        self.ValSage = self.Sage.GetValue()
+        self.ValSclass = self.Sclass.GetValue()
+        self.ValSdept = self.Sdept.GetValue()
+        self.ValSaddr = self.Saddr.GetValue()
+        self.ValSsex = self.Ssex.GetValue()
 
         hasPreAnd = 0
-        if len(self.ValSid) != 0 :
+        if len(self.ValSid) != 0:
             sqlDisplay += " Sid=" + "'" + self.ValSid + "'"
             hasPreAnd = 1
         if len(self.ValSname) != 0:
@@ -70,22 +72,22 @@ class MyFrame(wx.Frame):
                 sqlDisplay += " AND"
             sqlDisplay += " Sname=" + "'" + self.ValSname + "'"
             hasPreAnd = 1
-        if len(self.ValSage) != 0 :
+        if len(self.ValSage) != 0:
             if hasPreAnd == 1:
                 sqlDisplay += " AND"
             sqlDisplay += " Sage=" + "'" + self.ValSage + "'"
             hasPreAnd = 1
-        if len(self.ValSclass) != 0 :
+        if len(self.ValSclass) != 0:
             if hasPreAnd == 1:
                 sqlDisplay += " AND"
             sqlDisplay += " Sclass=" + "'" + self.ValSclass + "'"
             hasPreAnd = 1
-        if len(self.ValSdept) != 0 :
+        if len(self.ValSdept) != 0:
             if hasPreAnd == 1:
                 sqlDisplay += " AND"
             sqlDisplay += " Sdept=" + "'" + self.ValSdept + "'"
             hasPreAnd = 1
-        if len(self.ValSaddr) != 0 :
+        if len(self.ValSaddr) != 0:
             if hasPreAnd == 1:
                 sqlDisplay += " AND"
             sqlDisplay += " Saddr=" + "'" + self.ValSaddr + "'"
@@ -102,9 +104,9 @@ class MyFrame(wx.Frame):
             sqlDisplay += ';'
 
         self.sqlText.SetValue(sqlDisplay)
-        #search in MySQL:
+        # search in MySQL:
 
-        db = MySQLdb.connect("localhost", "root", "12345678", "labdb", charset = 'utf8')
+        db = MySQLdb.connect("localhost", "root", "12345678", "labdb", charset='utf8')
         cursor = db.cursor()
         sql = sqlDisplay
         # sql = sqlDisplay.encode('utf8')
@@ -116,14 +118,14 @@ class MyFrame(wx.Frame):
             resDisplayValue = ""
             print ("共%d条查询结果\n") % len(results)
             for row in results:
-                resSid    = row[0]          + "\t"
-                resSage   = unicode(row[1]) + "\t"
-                resSsex   = row[2]          + "\t"
-                resSdept  = row[3]          + "\t"
-                resSname  = row[4]          + "\t"
-                resSaddr  = row[5]          + "\t"
-                resSclass = row[6]          + "\n"
-                resDisplayValue += resSid+resSage+resSsex+resSdept+resSname+resSaddr+resSclass
+                resSid = row[0] + "\t"
+                resSage = unicode(row[1]) + "\t"
+                resSsex = row[2] + "\t"
+                resSdept = row[3] + "\t"
+                resSname = row[4] + "\t"
+                resSaddr = row[5] + "\t"
+                resSclass = row[6] + "\n"
+                resDisplayValue += resSid + resSage + resSsex + resSdept + resSname + resSaddr + resSclass
                 # print resDisplayValue
             self.ResDisplay.SetValue(resDisplayValue)
 
